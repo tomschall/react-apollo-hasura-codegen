b6ead871fa2fd19ab955d1849efd96f2697e4653
Thomas Schallert <thomas.schallert@fhnw.ch>
2021-11-05 16:11:20 +0100

    add strapi

---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '47f3468e-118d-40b2-8a1d-8d210bf615fb'
+};
--- src/User.tsx
+++ src/User.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useGetUsersQuery } from './api/generated/graphql';
+import {
+  useGetUsersSubscription,
+  useSomethingsSubscription,
+} from './api/generated/graphql';
 import Logout from './Logout';
 
 const User: React.FC = () => {
   const { isAuthenticated, isLoading } = useAuth0();
-  const { data, loading, error } = useGetUsersQuery({
-    variables: {},
-  });
+  const { data, loading, error } = useGetUsersSubscription();
 
-  if (isLoading || loading) {
+  const {
+    data: somethingData,
+    loading: loadingData,
+    error: errorData,
+  } = useSomethingsSubscription({});
+
+  if (isLoading || loading || loadingData) {
     return <>'loading yeah...'</>;
   }
 
-  if (error) {
+  if (error || errorData) {
     console.log('error', error);
   }
-
-  console.log('data', data);
 
   return (
     <>
@@ -27,11 +32,23 @@ const User: React.FC = () => {
             <Logout />
           </p>
           <p>users from database:</p>
-          <ul>
-            {data?.user.map((user, index) => {
-              return <li key={index}>hi {user.firstname}</li>;
-            })}
-          </ul>
+
+          {data?.strapi_administrator.map((admin, index) => {
+            return (
+              <ul key={index}>
+                <li>{admin.id}</li>
+                <li>{admin.firstname}</li>
+                <li>{admin.email}</li>
+                <li>{admin.isActive}</li>
+                <li>{admin.lastname}</li>
+                <li>{admin.password}</li>
+                <li>{admin.username}</li>
+                <li>{admin.resetPasswordToken}</li>
+                <li>{admin.registrationToken}</li>
+                <li>{admin.preferedLanguage}</li>
+              </ul>
+            );
+          })}
         </>
       ) : (
         <></>
